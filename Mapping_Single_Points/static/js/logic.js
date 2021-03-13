8e8c1507871caa5fe11719bb9621e7bd7b359320
renatabg2205 <renatabg@gmail.com>
2021-03-13 11:29:14 -0600

    Updating Mapping_Single_Points/static/js/logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 //An alternative to using the setView() method is to modify each attribute in the map object using the curly braces notation as follows:
 // Create the map object with a center and zoom level.
@@ -12,6 +12,34 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     ],
 //     zoom: 4
 //   });
+
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// L.circle([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     fillColor: "lightyellow",
+//     fillOpacity: "0.75"
+//  }).addTo(map);
+
+//Alternatively, we can create a circle using the circleMarker() function. 
+//The circleMarker() function measures the radius of the circle in pixels, with the default radius set at 10 pixels
+//Add a circle to the map
+L.circleMarker([34.0522, -118.2437],{
+    radius: 300,
+    color: "black",
+    fillColor: "#ffffa1"
+}).addTo(map);
+
+ // We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "dark-v10",
+        accessToken: API_KEY
+    });
+// // Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
 
 // We create the tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -26,10 +54,10 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //Use the Mapbox Styles API
 //Edit the URL and use the Styles API for the type of map you want.
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
 // // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
